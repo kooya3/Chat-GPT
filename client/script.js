0257ba73e61b96f25b777ee22b78b7c05256ae53
kooya3 <elyeestatua@gmail.com>
2023-03-16 19:49:46 +0300

    created the loader component and set the interval

--- client/script.js
+++ client/script.js
@@ -3,3 +3,19 @@ import user from './assets/user.svg'
 
 const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')
+
+let loadInterval;
+
+function loader(element) {
+    element.textContent = ''
+
+    loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
+        element.textContent += '.';
+
+        // If the loading indicator has reached three dots, reset it
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300);
+}
