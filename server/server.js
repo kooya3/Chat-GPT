b63296cd1dc91908f138ded1d8863333a79e99ed
kooya3 <elyeestatua@gmail.com>
2023-03-16 20:56:14 +0300

    setting up API config

--- server/server.js
+++ server/server.js
@@ -4,3 +4,22 @@ import cors from 'cors'
 import { Configuration, OpenAIApi } from 'openai'
 
 dotenv.config()
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  
+  const openai = new OpenAIApi(configuration);
+  
+  const app = express()
+  app.use(cors())
+  app.use(express.json())
+  
+  app.get('/', async (req, res) => {
+    res.status(200).send({
+      message: 'Hello from Lissa!'
+    })
+  })
+
+  app.post('/', async (req, res) => {
+  })
